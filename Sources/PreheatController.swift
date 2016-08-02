34bd22d4ce6e3ce94abe4ee1d6db5b079bc2aa29
kean <grebenyuk.alexander@gmail.com>
2016-08-02 11:35:25 +0300

    Update for Xcode 8 beta 4

--- Sources/PreheatController.swift
+++ Sources/PreheatController.swift
@@ -5,16 +5,16 @@
 import UIKit
 
 /// Automates prefetching of content in a scroll views. After creating a preheat controller you should enable it by setting `enabled` property to `true`.
-public class PreheatController<V where V: UIScrollView, V: PreheatingView> : NSObject {
+public class Controller<V where V: UIScrollView, V: Preheated> : NSObject {
     /// Gets called when the preheat window changes. Provides an array of index paths that were added and that were removed from the previous preheat window. Index paths are ordered by the distance to the viewport.
-    public var handler: ((added: [NSIndexPath], removed: [NSIndexPath]) -> Void)?
-
+    public var handler: ((added: [IndexPath], removed: [IndexPath]) -> Void)?
+    
     /// The view that the receiver was initialized with.
     public let view: V
-
+    
     /// Currently preheated index paths.
-    public private(set) var indexPaths = [NSIndexPath]()
-
+    public private(set) var indexPaths = [IndexPath]()
+    
     /// The proportion of the scroll view's width (or height for views with vertical orientation) used as a preheating window width (or height respectively).
     public var preheatRectSizeRatio: CGFloat = 1.0
     
@@ -34,18 +34,18 @@ public class PreheatController<V where V: UIScrollView, V: PreheatingView> : NSO
     }
     
     private var previousOffset: CGPoint?
-
+    
     deinit {
         view.removeObserver(self, forKeyPath: "contentOffset", context: nil)
     }
-
+    
     /// Initializes the receiver with a given view.
     public init(view: V) {
         self.view = view
         super.init()
-        self.view.addObserver(self, forKeyPath: "contentOffset", options: [.New], context: nil)
+        self.view.addObserver(self, forKeyPath: "contentOffset", options: [.new], context: nil)
     }
-
+    
     /// Removes all index paths without signalling the delegate. Then updates preheat rect (if enabled).
     public func reset() {
         indexPaths.removeAll()
@@ -54,14 +54,14 @@ public class PreheatController<V where V: UIScrollView, V: PreheatingView> : NSO
             update()
         }
     }
-
-    override public func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+    
+    override public func observeValue(forKeyPath keyPath: String?, of object: AnyObject?, change: [NSKeyValueChangeKey : AnyObject]?, context: UnsafeMutablePointer<Void>?) {
         if object === view {
             if enabled {
                 update()
             }
         } else {
-            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: nil)
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: nil)
         }
     }
     
@@ -71,8 +71,8 @@ public class PreheatController<V where V: UIScrollView, V: PreheatingView> : NSO
         }
         let isScrollingForward = view.isScrollingForward(previousOffset: previousOffset)
         let preheatRect = view.preheatingRect(isScrollingForward: isScrollingForward, sizeRatio: preheatRectSizeRatio)
-        let indexPaths = Set(view.indexPaths(in: preheatRect)).subtract(view.indexPathsForVisibleItems())
-        updateIndexPaths(indexPaths.sort({ // sort in scroll direction
+        let indexPaths = Set(view.indexPaths(in: preheatRect)).subtracting(view.indexPathsForVisibleItems)
+        updateIndexPaths(indexPaths.sorted(by: { // sort in scroll direction
             if isScrollingForward {
                 return $0.section < $1.section || $0.item < $1.item
             } else {
@@ -83,18 +83,18 @@ public class PreheatController<V where V: UIScrollView, V: PreheatingView> : NSO
     }
     
     private func shouldUpdatePreheatRect() -> Bool {
-        func distance(p1: CGPoint, _ p2: CGPoint) -> CGFloat {
+        func distance(_ p1: CGPoint, _ p2: CGPoint) -> CGFloat {
             let dx = p2.x - p1.x, dy = p2.y - p1.y
             return sqrt((dx * dx) + (dy * dy))
         }
         guard let previousOffset = previousOffset else {
             return true
         }
-        let margin = (view.orientation == .Vertical ? CGRectGetHeight : CGRectGetWidth)(view.bounds) * updateRatio
+        let margin = (view.orientation == .vertical ? view.bounds.height : view.bounds.width) * updateRatio
         return distance(view.contentOffset, previousOffset) > margin
     }
-
-    private func updateIndexPaths(newIndexPaths: [NSIndexPath]) {
+    
+    private func updateIndexPaths(_ newIndexPaths: [IndexPath]) {
         let added = newIndexPaths.filter { !indexPaths.contains($0) }
         let removed = indexPaths.filter { !newIndexPaths.contains($0) }
         indexPaths = newIndexPaths
@@ -102,71 +102,71 @@ public class PreheatController<V where V: UIScrollView, V: PreheatingView> : NSO
     }
 }
 
-private extension PreheatingView where Self: UIScrollView {
-    func preheatingRect(isScrollingForward isScrollingForward: Bool, sizeRatio: CGFloat) -> CGRect {
+private extension Preheated where Self: UIScrollView {
+    func preheatingRect(isScrollingForward: Bool, sizeRatio: CGFloat) -> CGRect {
         let viewport = CGRect(origin: contentOffset, size: bounds.size)
         switch orientation {
-        case .Vertical:
-            let height = CGRectGetHeight(viewport) * sizeRatio
-            let y = isScrollingForward ? CGRectGetMaxY(viewport) : CGRectGetMinY(viewport) - height
-            return CGRectIntegral(CGRect(x: 0, y: y, width: CGRectGetWidth(viewport), height: height))
-        case .Horizontal:
-            let width = CGRectGetWidth(viewport) * sizeRatio
-            let x = isScrollingForward ? CGRectGetMaxX(viewport) : CGRectGetMinX(viewport) - width
-            return CGRectIntegral(CGRect(x: x, y: 0, width: width, height: CGRectGetHeight(viewport)))
+        case .vertical:
+            let height = viewport.height * sizeRatio
+            let y = isScrollingForward ? viewport.maxY : viewport.minY - height
+            return CGRect(x: 0, y: y, width: viewport.width, height: height).integral
+        case .horizontal:
+            let width = viewport.width * sizeRatio
+            let x = isScrollingForward ? viewport.maxX : viewport.minX - width
+            return CGRect(x: x, y: 0, width: width, height: viewport.height).integral
         }
     }
     
-    func isScrollingForward(previousOffset previousOffset: CGPoint?) -> Bool {
+    func isScrollingForward(previousOffset: CGPoint?) -> Bool {
         guard let previousOffset = previousOffset else {
             return true
         }
         switch orientation {
-        case .Vertical: return contentOffset.y >= previousOffset.y
-        case .Horizontal: return contentOffset.x >= previousOffset.x
+        case .vertical: return contentOffset.y >= previousOffset.y
+        case .horizontal: return contentOffset.x >= previousOffset.x
         }
     }
 }
 
 
 public enum ScrollOrientation {
-    case Vertical, Horizontal
+    case vertical, horizontal
 }
 
-public protocol PreheatingView {
+public protocol Preheated {
     var orientation: ScrollOrientation { get }
-    func indexPaths(in rect: CGRect) -> [NSIndexPath]
-    func indexPathsForVisibleItems() -> [NSIndexPath]
+    func indexPaths(in rect: CGRect) -> [IndexPath]
+    var indexPathsForVisibleItems: [IndexPath] { get }
 }
 
 
-extension UICollectionView: PreheatingView {
+extension UICollectionView: Preheated {
     public var orientation: ScrollOrientation {
         switch (collectionViewLayout as! UICollectionViewFlowLayout).scrollDirection {
-        case .Vertical: return .Vertical
-        case .Horizontal: return .Horizontal
+        case .vertical: return .vertical
+        case .horizontal: return .horizontal
         }
     }
-
-    public func indexPaths(in rect: CGRect) -> [NSIndexPath] {
-        guard let attributes = collectionViewLayout.layoutAttributesForElementsInRect(rect) else {
+    
+    public func indexPaths(in rect: CGRect) -> [IndexPath] {
+        guard let attributes = collectionViewLayout.layoutAttributesForElements(in: rect) else {
             return []
         }
-        return attributes.filter{ $0.representedElementCategory == .Cell }.map{ $0.indexPath }
+        return attributes.filter{ $0.representedElementCategory == .cell }.map{ $0.indexPath }
     }
 }
 
 
-extension UITableView: PreheatingView {
+extension UITableView: Preheated {
     public var orientation: ScrollOrientation {
-        return .Vertical
+        return .vertical
     }
     
-    public func indexPaths(in rect: CGRect) -> [NSIndexPath] {
-        return indexPathsForRowsInRect(rect) ?? []
+    public func indexPaths(in rect: CGRect) -> [IndexPath] {
+        return indexPathsForRows(in: rect) ?? []
     }
-
-    public func indexPathsForVisibleItems() -> [NSIndexPath] {
+    
+    public var indexPathsForVisibleItems: [IndexPath] {
         return indexPathsForVisibleRows ?? []
     }
 }
